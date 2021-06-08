2ed3ac59d1647c8f331627949051e3ae890caa50
creativelogy <khalifah.wijaya4@student.uns.ac.id>
2021-06-08 22:57:16 +0700

    update indext test

--- resources/views/indexTest.blade.php
+++ resources/views/indexTest.blade.php
@@ -70,15 +70,15 @@
         <!--home bg---------------->
         <div class="home-background">
             <div class="main-text" style="margin-top:-230pt">
-                <h1 class="text-center font-weight-bold">We Provide The Best Health<br>Services For You</h1>
-                <p class="text-center lead" style="font-size: 14pt; margin-top:15pt">Lorem ipsum dolor sit amet, consectetur<br>adipiscing elit. Magna ut euismod</p>
+                <h1 class="text-center font-weight-bold headline-home">We Provide The Best Health<br>Services For You</h1>
+                <p class="text-center desc-home">Lorem ipsum dolor sit amet, consectetur<br>adipiscing elit. Magna ut euismod</p>
                 <!--btn------->
                 <button type="button" class="btn btn-success mx-auto d-block login-btn" style="border-radius: 20pt; margin-top:15pt">Reservation</button>
             </div>
         </div>
         <div style="margin-top:-168pt">
             <h2 class="text-center titlehome">Powered By</h2>
-            <p class="text-center text-muted lead" style="font-size: 12pt; margin-top:5pt;">1000+ Trusted Medical Organization Throughout Indonesia</p>
+            <p class="text-center text-muted desc-powered">1000+ Trusted Medical Organization Throughout Indonesia</p>
             <img class="rounded mx-auto d-block poweredlogo" src="../img/powered logo.png"  alt="">
         </div>
         
