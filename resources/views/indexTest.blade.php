43d2d0306ff2187c39a922c44e9d3578fe4740dd
Krisna Sanjaya M0519055 <58094843+krisnassjy@users.noreply.github.com>
2021-06-09 19:07:30 +0700

    Membuat layout baru

--- resources/views/indexTest.blade.php
+++ resources/views/indexTest.blade.php
@@ -1,72 +1,7 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <title>Klinik Pratama | Home</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="shortcut icon" href="../img/logoonly.png">
-    <link rel="stylesheet" type="text/css" href="../css/indexTest.css">
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  </head>
-  <body>
-    <div id="stickynav">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                    <div class="container-fluid">
-                        <a class="navbar" href="#">
-                            <img style="margin-left:10pt" src="../img/logo-nav.png" alt="" width="70%" height="25">
-                        </a>
-                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                        </button>
-                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Home</a>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                Products
-                                </a>
-                                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                                    <a class="dropdown-item" href="#">Action</a>
-                                    <a class="dropdown-item" href="#">Another action</a>
-                                    <a class="dropdown-item" href="#">Something else here</a>
-                                </div>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Services
-                                </a>
-                                <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                                    <li><a class="dropdown-item" href="#">Action</a></li>
-                                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                                </ul>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">About</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" 
-                                href="https://api.whatsapp.com/send?phone=628977887994&text=Saya%20ingin%20melakukan%20reservasi%20di%20Klinik%20Pratama!"
-                                target="_blank">
-                                Contact
-                                </a>
-                            </li>
-                        </ul>
-                        </div>
-                        <form class="d-flex button-set">
-                            <button class="btn mr-2 login-btn btn-bulat" style="border-radius: 50pt; width:85px;" type="submit">Login</button>
-                            <button class="btn btn-outline-dark btn-bulat" style="border:1px solid black; border-radius: 50pt; width:85px;" type="submit">Sign Up</button>
-                        </form> 
-                    </div>
-        </nav>
-    </div>
-    
-    <!--Content---------------->
-    <div class="content">
+@extends('layout/main')
+
+@section('container')
+<div class="content">
         <!--home bg---------------->
         <div class="home-background">
             <div class="main-text" style="margin-top:-230pt">
@@ -84,19 +19,4 @@
         
         <!--text---------------->
     </div>
-    <h1 style="margin-top:25pt;">test</h1>
-    <h2>test</h2>
-    <h3>test</h3>
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <!-- jQuery -->
-    <script src="js/jquery-3.4.1.min.js"></script>
-
-    <!-- Popper -->
-    <script src="js/popper.min.js"></script>
-
-    <!-- Bootstrap js -->
-    <script src="js/bootstrap.min.js"></script>
-    
-</body>
-</html>
+  @endsection
--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -1,64 +1,82 @@
-<!DOCTYPE html>
-<html>
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Klinik Pratama | Home</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="shortcut icon" href="../img/logoonly.png">
+    <link rel="stylesheet" type="text/css" href="../css/indexTest.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+    <div id="stickynav">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <div class="container-fluid">
+                        <a class="navbar" href="#">
+                            <img style="margin-left:10pt" src="../img/logo-nav.png" alt="" width="70%" height="25">
+                        </a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Products
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                    <a class="dropdown-item" href="#">Action</a>
+                                    <a class="dropdown-item" href="#">Another action</a>
+                                    <a class="dropdown-item" href="#">Something else here</a>
+                                </div>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Services
+                                </a>
+                                <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                    <li><a class="dropdown-item" href="#">Action</a></li>
+                                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                </ul>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">About</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" 
+                                href="https://api.whatsapp.com/send?phone=628977887994&text=Saya%20ingin%20melakukan%20reservasi%20di%20Klinik%20Pratama!"
+                                target="_blank">
+                                Contact
+                                </a>
+                            </li>
+                        </ul>
+                        </div>
+                        <form class="d-flex button-set">
+                            <button class="btn mr-2 login-btn btn-bulat" style="border-radius: 50pt; width:85px;" type="submit">Login</button>
+                            <button class="btn btn-outline-dark btn-bulat" style="border:1px solid black; border-radius: 50pt; width:85px;" type="submit">Sign Up</button>
+                        </form> 
+                    </div>
+        </nav>
+    </div>
+    
+    <!--Content---------------->
+    @yield('container');
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <!-- jQuery -->
+    <script src="js/jquery-3.4.1.min.js"></script>
 
-<head>
-	<title>Home</title>
-	<link rel="stylesheet" type="text/css" href="/css/filestyle.css">
-    <link rel="stylesheet" href="/css/bootstrap.min.css">
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-</head>
+    <!-- Popper -->
+    <script src="js/popper.min.js"></script>
 
-<body>
-	<!-- <div id="tab">
-		<div id="kotak">
-			<h1>KLINIK<img src="icon1.png" alt="icon klinik" style="width: 30px; height: 30px;">PRATAMA</h1>
-		</div>
-	</div>
-	
-	<hr>
-	<div class="kotakbawah">	
-		<img src="klinik pratama.jpg" alt="doctor" style="width: 800px;height: 430px;">
-	</div> -->
-
-	<nav class="navbar navbar-expand-lg navbar-light bg-light">
-		<h2>Klinik Pratama</h2>
-		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
-			aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-		</button>
-		<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-			<div class="navbar-nav ml-auto">
-				<a class="nav-link active" href="/">Home </a>
-				<a class="nav-link" href="/pasien">Pasien</a>
-				<a class="nav-link" href="/perawat">Perawat</a>
-			</div>
-		</div>
-	</nav>
-
-	<div class="jumbotron jumbotron-fluid">
-		<div class="container">
-			<h1 class="display-4">Klinik Pratama</h1>
-			<p class="lead">Jl. Piknik Pagi no 82, Kec. Hutan Rimba, Kota Surakarta</p>
-		</div>
-	</div>
-
-	<div class="container">
-        @yield('container');
-		<div class="row footer">
-            <div class="col text-center">
-                <p>© 2021 Copywriter by Kelompok 5</p>
-            </div>
-        </div>
-	</div>
-
-	<!-- <div class="wrapper">
-		<img src="tirta.jpg" alt="dr.tirta" style="width: 50%; height: 50%;">
-		<h2>dr.Dewa Kipas</h2>
-		<p>spesialis pijet memijet
-			<br>
-			Jam 7:00-15:00 WIB
-		</p>
-	</div> -->
+    <!-- Bootstrap js -->
+    <script src="js/bootstrap.min.js"></script>
+    
 </body>
-
 </html>
