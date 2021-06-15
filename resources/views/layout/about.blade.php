d1ff8c03bc68799d74ac2892fb4a32b7a0773866
creativelogy <khalifah.wijaya4@student.uns.ac.id>
2021-06-15 19:12:45 +0700

    Tes ubah dropdown

--- resources/views/layout/about.blade.php
+++ resources/views/layout/about.blade.php
@@ -42,7 +42,7 @@
                                 </div>
                             </li>
                             <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="true">
                                     Services
                                 </a>
                                 <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
