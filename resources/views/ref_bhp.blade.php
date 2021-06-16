bcd4409b34b420e07f8bb7c1d93db3b3ba117351
Ghoza Nada Iqbal <ghozanadaiqbal@student.uns.ac.id>
2021-06-16 23:11:36 +0700

    bhp catalog belum jadi
    belum ketemu referensi

--- resources/views/ref_bhp.blade.php
+++ resources/views/ref_bhp.blade.php
@@ -1,39 +1,45 @@
 @extends('layout/main')
 
-@section('title','BHP')
-
+@section('title', 'BHP')
+    <link rel="stylesheet" type="text/css" href="../css/ref_bhp.css">
 @section('container')
-<div class="container">
-    <div class="row">
-        <div class="col-12">
-         <h1 class="mt-3">List</h1>
-         <table class="table">
-            <thead class="table-dark">
-                <tr>
-                <th scope="col">ID</th>
-                <th scope="col">Nama</th>
-                <th scope="col">Harga</th>
-                <th scope="col">Created By</th>
-                <th scope="col">Edited By</th>
-                <th scope="col">Created At</th>
-                <th scope="col">Updated At</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($ref_bhp as $prwt)
-                <tr>
-                    <th scope="row">{{$loop-> iteration}}</th>
-                    <td>{{$prwt-> nama}}</td>
-                    <td>{{$prwt-> harga}}</td>
-                    <td>{{$prwt-> created_by}}</td>
-                    <td>{{$prwt-> edited_by}}</td>
-                    <td>{{$prwt-> created_at}}</td>
-                    <td>{{$prwt-> updated_at}}</td>
-                </tr>
-                @endforeach
+    <div class="container">
+        <div class="row">
+            <div class="col-12"><br><br><br><br>
+                <h1 class="mt-3">List</h1>
+                <table class="table">
+
+                    <tbody>
+                        @foreach ($ref_bhp as $prwt)
+                            <tr>
+                                {{-- <th scope="row">{{$loop-> iteration}}</th> --}}
+                                <div class="border">
+                                    <td><img src="{{ $prwt->img_dir }}" class="isi catalog"> <br>
+                                        <div class="name">
+                                            {{ $prwt->nama }} <br><br>
+                                        </div>
+                                        {{-- <div class="harga">
+                                            {{ $prwt->harga }}
+                                        </div> --}}
+                                    </td>
+                                </div>
+            </div>
+            </tr>
+            @endforeach
             </tbody>
-         </table>
+            </table>
         </div>
     </div>
-</div>
+    {{-- <tbody>
+    @foreach ($ref_bhp as $prwt)
+    {{$loop-> iteration}}
+    <img src="{{$prwt-> img_dir}}">
+    {{$prwt-> nama}}
+    {{$prwt-> harga}}
+    @endforeach
+    </tbody> --}}
+
+
+
+    </div>
 @endsection
