8bd0adc4c0f6c05fb66a71ceeda90a1d177fa674
Ghoza Nada Iqbal <ghozanadaiqbal@student.uns.ac.id>
2021-06-24 06:22:12 +0700

    Revert "Giving comment product.bhp"
    This reverts commit ff8b8ae60c72dd3f84778d187e6638d2c2349a8d.

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,81 +1,40 @@
 @extends('layout/mainProduct')
 
 @section('title', 'Products')
-    <link rel="stylesheet" type="text/css" href="../css/product.css">
-    {{-- <link rel="stylesheet" type="text/css" href="../css/reservasi.css"> --}}
+<link rel="stylesheet" type="text/css" href="../css/product.css">
 
 @section('container')
 
-    <div class="toppage">
-        <h1>SAFE MEDICINE AND TRUSTED</h1>
-    </div>
-    <div class="d-flex d-row justify-content-between mt-5">
-        <div class="d-flex flex-column justify-content-center m-5">
-            <div class="medicine">
-                <img src="../img/obat.png" alt="obat">
-            </div>
+<div class="toppage">
+    <h1>SAFE MEDICINE AND TRUSTED</h1>
+</div>
+<div class="d-flex d-row justify-content-between mt-5">
+    <div class="d-flex flex-column justify-content-center m-5">
+        <div class="medicine">
+            <img src="../img/obat.png" alt="obat">
+        </div>
 
-            <div class="butmedicine">
-                <div class="linktext">
-                    <a href="ref_obat">
-                        <p>Medicine</p>
-                    </a>
-                </div>
+        <div class="butmedicine">
+            <div class="linktext">
+                <a href="ref_obat"><p>Medicine</p></a>
             </div>
         </div>
+    </div>
 
 
-        <div class="d-flex flex-column justify-content-center m-5">
-            <div class="bhp">
-                <img src="../img/apd 1.png" alt="bhp">
-            </div>
+    <div class="d-flex flex-column justify-content-center m-5">
+        <div class="bhp">
+            <img src="../img/apd 1.png" alt="bhp">
+        </div>
 
-            <div class="butbhp">
-                <div class="linktext2">
-                    <a href="ref_bhp">
-                        <p>BHP</p>
-                    </a>
-                </div>
+        <div class="butbhp">
+            <div class="linktext2">
+                <a href="ref_bhp"><p>BHP</p></a>
             </div>
         </div>
-
     </div>
 
-    {{-- <div class="toppage">
-        <h1>Klinik Pratama</h1>
-        <div class="h4geser">
-            <h4>SAFE MEDICINE AND TRUSTED</h4>
-        </div>
-    </div>
-
-    <div class="tambah">
-        <img src="../img/obat.png" alt="tambah">
-    </div>
-
-    <div class="buttambah">
-        <div class="linktext">
-            <a href="ref_obat">
-                <div class="medgeser">
-                    <p>Medicine</p>
-                </div>
-            </a>
-        </div>
-    </div>
-
-    <div class="lihat">
-        <img src="../img/apd 1.png" alt="lihat">
-    </div>
-
-    <div class="butlihat">
-        <div class="linktext2">
-            <a href="ref_bhp">
-                <div class="bhpgeser">
-                    <p>BHP</p>
-                </div>
-            </a>
-        </div>
-    </div> --}}
-
+</div>
 
 
 
